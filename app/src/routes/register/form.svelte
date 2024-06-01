b231f725b4c96faecef669f24c4ca17d91ae2271
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-06-01 14:40:45 +0200

    feat: login and register

---
+++ app/src/routes/register/form.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { Description } from 'formsnap';
+	import { formSchema, type FormSchema } from './schema';
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export let data: SuperValidated<Infer<FormSchema>>;
+
+	const form = superForm(data, {
+		validators: zodClient(formSchema)
+	});
+
+	const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance>
+	<Form.Field {form} name="email">
+		<Form.Control let:attrs>
+			<Form.Label>Email</Form.Label>
+			<Input {...attrs} bind:value={$formData.email} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="fname">
+		<Form.Control let:attrs>
+			<Form.Label>First name</Form.Label>
+			<Input {...attrs} bind:value={$formData.fname} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="lname">
+		<Form.Control let:attrs>
+			<Form.Label>Last name</Form.Label>
+			<Input {...attrs} bind:value={$formData.lname} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="phone">
+		<Form.Control let:attrs>
+			<Form.Label>Phone number</Form.Label>
+			<Input {...attrs} bind:value={$formData.phone} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="address">
+		<Form.Control let:attrs>
+			<Form.Label>Address</Form.Label>
+			<Input {...attrs} bind:value={$formData.address} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="password">
+		<Form.Control let:attrs>
+			<Form.Label>Password</Form.Label>
+			<Input {...attrs} bind:value={$formData.password} />
+		</Form.Control>
+		<Form.Description />
+		<Form.FieldErrors />
+	</Form.Field>
+	<p class="my-2">
+		Already have an account? <a class="hover:underline" href="/login">Login</a>
+	</p>
+	<Form.Button>Register</Form.Button>
+</form>
---
+++ app/src/routes/register/schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const formSchema = z.object({
+	email: z.string().email(),
+	fname: z.string().min(1).max(64),
+	lname: z.string().min(1).max(64),
+	phone: z.string().min(9).max(11),
+	address: z.string().min(1).max(128),
+	password: z.string().min(8).max(128)
+});
+
+export type FormSchema = typeof formSchema;
